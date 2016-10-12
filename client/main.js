662b6e87163694f869e7b448e9dbe0cf3324f093
root <tim.asscherickx@hotmail.com>
2016-10-12 15:01:04 +0200

    test

--- client/main.js
+++ client/main.js
@@ -1,15 +1,10 @@
 
-
-
 //   <!-- angular dependencies -->
 import angular from 'angular';
 
 //    <!-- application code -->
 import './js/app.js';
-import './js/controllers.js';
 
 import './css/style.css';
 import './css/bootstrap.min.css';
 import './main.html';
-
-
