99bd88d09932a2750cb3b35ac74e3e310c847e57
Jeroen <jeroenvanwolput1994@outlook.com>
2016-10-20 12:14:02 +0200

    Lessen toevoeg knop toegevoegd + folder voor de collections (database folder)

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
-
 //   <!-- angular dependencies -->
-import angular from 'angular';
+//import angular from 'angular';
 
 //    <!-- application code -->
 import './js/app.js';
@@ -8,3 +7,23 @@ import './js/app.js';
 import './css/style.css';
 import './css/bootstrap.min.css';
 import './main.html';
+
+Template.OverzichtLessen.helpers({
+	historyLes: function(){
+		return Lessen.find();
+		//console.log("erin");
+	}
+});
+
+Template.OverzichtLessen.events({
+	'click #addLes' : function(e){
+		e.preventDefault();
+
+		var lesTitel = template.find('#TitelLes').value;
+
+		Lessen.insert({
+			lesnaam: lesTitel
+		});
+		OverzichtLessen.update(Lessen);
+	}
+});
