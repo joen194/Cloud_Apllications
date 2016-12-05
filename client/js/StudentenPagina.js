354ce0895e5d0351a52cfc155b3b91735be2280b
Steven <stevenverheyen1@gmail.com>
2016-12-05 16:06:19 +0100

    Aanpassing antwoorden

--- client/js/StudentenPagina.js
+++ client/js/StudentenPagina.js
@@ -3,16 +3,16 @@
 Template.StudentenPagina.events({
 		'click #submitAntwoord': function(e){
 		e.preventDefault();
-		var tijdelijkeVraagId = "Njirz9PG7emjoQYqz";
+
+		var tijdelijkeVraagId = Lessen.find({roomCode: roomCode}).fetch();
 		var naam = $('#naamInput').val();
 		var tijdelijkAntwoord = $('#antwoordField').val();
 
-
-		Meteor.call('AntwoordToevoegen', tijdelijkeVraagId, tijdelijkAntwoord, naam, function(error, res){
+		Meteor.call('AntwoordToevoegen', tijdelijkeVraagId[0].vraagId, tijdelijkAntwoord, naam, function(error, res){
 		if (error)
 			return alert(error.reason);
 		});
-		console.log("gelukt");
+
 	},
 //******************************** Code die de naam in de database zet ******************************
 	'click #enterName': function(e) {
@@ -38,27 +38,19 @@ Template.StudentenPagina.events({
 
 Template.StudentenPagina.helpers ({
 	oneMultipleChoice: function (){
-		console.log("in helpers");
 		var roomCode = window.location.hash.substr(1);
 		var tijdelijkeDbStudentenPagina = Lessen.find({roomCode: roomCode}).fetch();
 		return MultipleChoice.find({vragenId: tijdelijkeDbStudentenPagina[0].vraagId});
 	},
 	openAntwoord: function(){
-		console.log("in get");
-		
-
 		var roomCode = window.location.hash.substr(1);
 		var tijdelijkeDbStudentenPagina = Lessen.find({roomCode: roomCode}).fetch();
-		console.log(tijdelijkeDbStudentenPagina);
 		var checkOpenVraag = Vragen.find({_id: tijdelijkeDbStudentenPagina[0].vraagId}).fetch();
 	
-		console.log(checkOpenVraag[0].openVraag);
 		if (checkOpenVraag[0].openVraag == true){
-			console.log("hey");
 			return true;
 		}
 		else {
-			console.log("multi");
 			return false;
 		}		
 	}, 
@@ -67,14 +59,9 @@ Template.StudentenPagina.helpers ({
 });
 
 function showAntwoordInput() {
-
-	console.log("SHow Antwoord input");
 	$("#NaamInputDiv").fadeOut(500);
 
 	setTimeout(function(){
  		$("#AntwoordInputDiv").fadeIn(500);
 	}, 500);
-
-	 	
-
 }
--- client/js/vraagOpBord.js
+++ client/js/vraagOpBord.js
@@ -10,15 +10,13 @@ Template.vraagOpBord.helpers({
 		return Vragen.find({_id: db[0].vraagId});
 	},
 	antwoordOpBord : function(){
-		console.log("helaba");
+
 		roomCode = window.location.hash.substr(1);
 		var db = Lessen.find({roomCode: roomCode}).fetch();
 
-		var dbVragen = Vragen.find({_id: db[0].vraagId}).fetch();
-		//console.log(dbVragen);
-	
+		var dbVragen = Vragen.find({_id: db[0].vraagId}).fetch();	
 		var dbAntwoorden = Antwoorden.find().fetch();
-		console.log("azetqsdf" + dbAntwoorden);
+
 		return Antwoorden.find({vraagId: dbVragen[0]._id});
 	}
 });
