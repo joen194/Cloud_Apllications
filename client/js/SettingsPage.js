dd248d90a6d82d9f2754d677f3cb98ccb22274ee
tim asscherickx <tim.asscherickx@hotmail.com>
2017-01-10 23:07:48 +0100

    settings page done

--- client/js/SettingsPage.js
+++ client/js/SettingsPage.js
@@ -1,4 +1,6 @@
-
+var rotate1= false;
+var rotate2= false;
+var rotate3= false;
 Template.SettingsPage.events({
     'click #RemoveUser': function(e){
         console.log(Meteor.userId());
@@ -36,8 +38,34 @@ Template.SettingsPage.events({
         }
     },
     'click #dropdownimg1': function(e){
-        //$("#dropdownimg1").rotate(180);
+        if (rotate1) {
+           $("#dropdownimg1").rotate(0); 
+           rotate1=false;
+       }else{
+            $("#dropdownimg1").rotate(180); 
+            rotate1 =true;
+       }   
         Meteor.myFunctions.DropDownMenu("dropdowndiv1","dropdownimg1");
+    },
+    'click #dropdownimg2': function(e){
+        if (rotate2) {
+           $("#dropdownimg2").rotate(0); 
+           rotate2=false;
+       }else{
+            $("#dropdownimg2").rotate(180); 
+            rotate2 =true;
+       }   
+        Meteor.myFunctions.DropDownMenu("dropdowndiv2","dropdownimg2");
+    },
+    'click #dropdownimg3': function(e){
+        if (rotate3) {
+           $("#dropdownimg3").rotate(0); 
+           rotate3=false;
+       }else{
+            $("#dropdownimg3").rotate(180); 
+            rotate3 =true;
+       }   
+        Meteor.myFunctions.DropDownMenu("dropdowndiv3","dropdownimg3");
     }
 
 
